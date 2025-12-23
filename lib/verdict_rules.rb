0e8c06a987d04a8edc0d572a769ff50db9ec9ab5
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-23 08:46:11 -0300

    feat(engine): cria class Engine em arquivo separado

--- lib/verdict_rules.rb
+++ lib/verdict_rules.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "verdict_rules/version"
+require_relative "verdict_rules/engine"
 
 module VerdictRules
   class Error < StandardError; end
-  
-  class Engine
-    
-  end
 end
