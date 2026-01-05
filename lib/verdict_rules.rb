0fabd121aa97cc44d530bd5f19f1fa8267d9e4f1
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2026-01-05 17:42:45 -0300

    feat(result): adiciona a class Result ao lib/verdict_rules.rb

--- lib/verdict_rules.rb
+++ lib/verdict_rules.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "verdict_rules/version"
+require_relative "verdict_rules/result"
 require_relative "verdict_rules/engine"
 require_relative "verdict_rules/rule"
 
