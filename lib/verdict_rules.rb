a9025c6d8cf5f92ce5bc8f66de4c90444e63f677
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2026-02-03 22:04:11 -0300

    feat(dsl): adicionar rule_builder ao lib/verdict_rule.rb

--- lib/verdict_rules.rb
+++ lib/verdict_rules.rb
@@ -2,8 +2,9 @@
 
 require_relative "verdict_rules/version"
 require_relative "verdict_rules/result"
-require_relative "verdict_rules/engine"
 require_relative "verdict_rules/rule"
+require_relative "verdict_rules/rule_builder"
+require_relative "verdict_rules/engine"
 
 module VerdictRules
   class Error < StandardError; end
